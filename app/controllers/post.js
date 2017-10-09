db8d4eb7f5be3358dfda2ef7f70a019d8a6f857e
allantorres92 <allantorres92@gmail.com>
2017-10-08 22:32:33 -0600

    second

--- app/controllers/post.js
+++ app/controllers/post.js
@@ -4,11 +4,19 @@ export default Ember.Controller.extend({
 	actions:{
 		sayHello: function(){
 			alert('Hello');
+		},
+		toggleBody: function(){
+			this.toggleProperty('isShowingBody');
+		},
+		submitAction: function(){
+			alert(this.get('name') + '    ' + this.get('comment'));
+
 		}
 	},
 	title: 'My Blog Post',
 	body: 'This is the body of the blog post',
 	authors:['William','Allan', 'Michelle'],
+	created: new Date(),
 	comments: [
 		{
 			name: 'Mike Smith',
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,7 @@ module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
     // Add options here
   });
+  app.import('node_modules/moment/min/moment.min.js')
 
   // Use `app.import` to add additional libraries to the generated
   // output files.
